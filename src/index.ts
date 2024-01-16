15d2cad241dd090575711f4879af4d65b7b1fa9e
lh <buffpal2016@gmail.com>
2024-01-16 09:57:04 -0500

    fix: 修复包导出问题

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-export * from "./components/GroupItem/index";
+import GroupItem from "./components/GroupItem/index";
+import GroupList from "./components/GroupList/index";
+
 export * from "./plugins/useNewMessagesPlugin";
 export * from "./plugins/useSelectsPlugin";
 export * from "./plugins/GroupListPlugin";
+export * from "./context/AntdApiContext";
+export * from "./context/I18nContext";
 export type { PluginType } from "./types/index";
+
+export { GroupItem, GroupList };
